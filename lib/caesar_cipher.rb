e02cb6217d96ef51b58539c051f1bae11095e108
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-16 00:13:03 +0300

    adding all of these stuff about the word_count

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,35 +1,35 @@
 # Our ascii method in ruby
 def caesar_cipher(sentence, number)
-    ascii = Array.new
-    a = Array.new
-    b = Array.new
-    number = number.to_i
-    sentence = sentence.split("")
-    for i in sentence do
-        i = i.ord 
-        ascii.push(i)
+  ascii = Array.new
+  a = Array.new
+  b = Array.new
+  number = number.to_i
+  sentence = sentence.split("")
+  for i in sentence do
+    i = i.ord 
+    ascii.push(i)
+  end
+  for i in ascii do
+    if i >= 97 && i < 123
+      i += number
+      if i >= 123
+        i = 97 + (i - 123)
+      end
+      a.push(i)
+    elsif i >= 65 && i < 91
+      i += number
+      if i >= 91
+        i = 65 + (i - 91)
+      end
+      a.push(i)
+    else
+      a.push(i)
     end
-    for i in ascii do
-        if i >= 97 && i < 123
-            i += number
-            if i >= 123
-                i = 97 + (i - 123)
-            end
-            a.push(i)
-        elsif i >= 65 && i < 91
-            i += number
-            if i >= 91
-                i = 65 + (i - 91)
-            end
-            a.push(i)
-        else
-            a.push(i)
-        end
-    end
-    for i in a do
-      i = i.chr
+  end
+  for i in a do
+    i = i.chr
       b.push(i) 
-    end
-    return b.join("")
+  end
+  return b.join("")
 end
-puts caesar_cipher("XYZxyza ...!", 1)
+puts caesar_cipher("What a string!", 5)
--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,4 +1,29 @@
 # word counter in ruby
 def word_counter(word, dictionary)
-  
+  hash = Hash.new
+  b = Array.new
+  j = 0
+  i = 0
+  a = 0
+  n = 0
+  word_one = word.split("!")
+  word_two = word_one.join(" ").split("?")
+  word_three = word_two.join(" ").split(",")
+  words = word_three.join(" ").split(" ")
+    while i < words.length do
+        while n < dictionary.length do
+          if words[a].downcase == dictionary[n].downcase
+            hash[dictionary[n]] = j + 1
+          end
+          n += 1
+        end
+        n = 0 
+        a += 1
+        i += 1
+    end
+    puts hash
 end
+  
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+puts word_counter("below below", dictionary)
+puts word_counter("Howdy partner, sit down! How's it going?", dictionary)
