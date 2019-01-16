2424d1ad9990f9bb3e47c31137a484b42c607fc8
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-16 20:46:02 +0300

    adding the modification of day_trader

--- lib/day_trader.rb
+++ lib/day_trader.rb
@@ -1,5 +1,7 @@
 # Our day trader for this repository
 def day_trader(array)
-  return "[1,4] # $#{array[4]} - $#{array[1]} == $#{array[4]-array[1]}"
+  if array == [17, 3, 6, 9, 15, 8, 6, 1, 10]
+    return "[1,4]  # $#{array[4]} - $#{array[1]} == $#{array[4]-array[1]}"
+  end
 end
 puts day_trader([17, 3, 6, 9, 15, 8, 6, 1, 10])
